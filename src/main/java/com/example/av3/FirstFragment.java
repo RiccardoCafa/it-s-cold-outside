0746fec90d07762fb93fcb8d12533665b26ac74e
Riccardo Cafagna <riccafagna@gmail.com>
2022-06-17 21:51:15 -0300

    Integration

--- src/main/java/com/example/av3/FirstFragment.java
+++ src/main/java/com/example/av3/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,7 +12,19 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.av3.databinding.FragmentFirstBinding;
 
+import Database.WeatherDAO;
+import Entities.WeatherCache;
+
 public class FirstFragment extends Fragment {
+
+    private WeatherDAO dao;
+
+    private TextView CityText;
+    private TextView TempText;
+    private TextView PrecipText;
+    private TextView WindText;
+    private TextView LatText;
+    private TextView LonText;
 
     private FragmentFirstBinding binding;
 
@@ -29,6 +42,17 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        dao = new WeatherDAO(view.getContext());
+
+        CityText = (TextView)view.findViewById(R.id.city_text);
+        TempText = (TextView)view.findViewById(R.id.temp_text);
+        PrecipText = (TextView)view.findViewById(R.id.precip_text);
+        WindText = (TextView)view.findViewById(R.id.vento_text);
+        LatText = (TextView)view.findViewById(R.id.lat_text);
+        LonText = (TextView)view.findViewById(R.id.lon_text);
+
+        setActiveCache();
+
         binding.reloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +60,27 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+    }
+
+    private void setActiveCache() {
+        int id = dao.getActiveWeather();
+
+        if (id != -1) {
+            WeatherCache cache = dao.GetById(id);
+            CityText.setText(cache.Name);
+            TempText.setText(cache.TempC + " °C");
+            PrecipText.setText(cache.PrecipMm + " mm");
+            WindText.setText(cache.WindMph + " Mph");
+            LatText.setText(cache.Latitude + "°");
+            LonText.setText(cache.Longitude + "°");
+        } else {
+            CityText.setText("--");
+            TempText.setText("-- °C");
+            PrecipText.setText("-- mm");
+            WindText.setText("-- Mph");
+            LatText.setText("-- °");
+            LonText.setText("-- °");
+        }
     }
 
     @Override
--- src/main/java/com/example/av3/MainActivity.java
+++ src/main/java/com/example/av3/MainActivity.java
@@ -31,34 +31,14 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
-    private WeatherDAO dao;
-
-    private TextView CityText;
-    private TextView TempText;
-    private TextView PrecipText;
-    private TextView WindText;
-    private TextView LatText;
-    private TextView LonText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        dao = new WeatherDAO(this);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
-
-        CityText = (TextView)this.findViewById(R.id.city_text);
-        TempText = (TextView)this.findViewById(R.id.temp_text);
-        PrecipText = (TextView)this.findViewById(R.id.precip_text);
-        WindText = (TextView)this.findViewById(R.id.vento_text);
-        LatText = (TextView)this.findViewById(R.id.lat_text);
-        LonText = (TextView)this.findViewById(R.id.lon_text);
-
-        setActiveCache();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -71,39 +51,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    private void setActiveCache() {
-        int id = dao.getActiveWeather();
-
-        if (id != -1) {
-            WeatherCache cache = dao.GetById(id);
-            CityText.setText(cache.Name);
-            TempText.setText(cache.TempC + " °C");
-            PrecipText.setText(cache.PrecipMm + " mm");
-            WindText.setText(cache.WindMph + " Mph");
-            LatText.setText(cache.Latitude + "°");
-            LonText.setText(cache.Longitude + "°");
-        } else {
-            CityText.setText("--");
-            TempText.setText("--" + " °C");
-            PrecipText.setText("--" + " mm");
-            WindText.setText("--" + " Mph");
-            LatText.setText("--" + "°");
-            LonText.setText("--" + "°");
-        }
-    }
-    
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        setActiveCache();
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        setActiveCache();
     }
 
     @Override
